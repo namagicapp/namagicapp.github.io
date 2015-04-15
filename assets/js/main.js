1a030a7a2750eafb21ec19205d2573eed585d46c
Johan <contact@johansatge.fr>
2015-04-15 20:28:48 +0200

    Fixed menu on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,19 @@
     'use strict';
 
     var module = {};
+    var $menu;
+    var $window;
+    var menuTop = 0;
 
     /**
      * Inits main module
      */
     module.init = function()
     {
+        $menu = $('.js-menu');
+        menuTop = $menu.offset().top;
+        $window = $(window);
+        $window.on('scroll', _onWindowScroll).trigger('scroll');
         $('.js-caption-magnifier').on('mouseover mouseout', _onCaptionMagnifier);
         $('.js-scrollable').on('click', _onSmoothScroll);
     };
@@ -33,8 +40,25 @@
     var _onSmoothScroll = function(evt)
     {
         evt.preventDefault();
-        var top = $($(evt.currentTarget).attr('href')).offset().top;
+        var top = $($(evt.currentTarget).attr('href')).offset().top - $menu.height();
         $('html, body').animate({scrollTop: top}, 300);
+    };
+
+    /**
+     * Updates menu state on scroll
+     */
+    var _onWindowScroll = function()
+    {
+        var window_top = $window.scrollTop();
+        var is_fixed = $menu.hasClass('js-fixed');
+        if (is_fixed && window_top <= menuTop)
+        {
+            $menu.removeClass('js-fixed');
+        }
+        if (!is_fixed && window_top > menuTop)
+        {
+            $menu.addClass('js-fixed');
+        }
     };
 
     window.Main = module;
